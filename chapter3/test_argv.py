ef558d6f339e81a8b5026ff6258789e416a08952
MiracleWong <cfwr1991@126.com>
2017-09-25 14:01:25 +0800

    add the test_argv file

--- chapter3/test_argv.py
+++ chapter3/test_argv.py
@@ -6,4 +6,17 @@ import sys
 import os
 
 # 不加任何参数时，打印的就是程序的文件名
-print(sys.argv)
+# print(sys.argv)
+
+def main():
+    sys.argv.append("")
+    filename = sys.argv[1]
+    if not os.path.isfile(filename):
+        raise SystemExit(filename + ' does not exits')
+    elif os.access(filename, os.R_OK):
+        raise SystemExit(filename + ' is not accessible')
+    else:
+        print(filename + ' is accessible')
+
+if __name__ == '__main__':
+    main()
